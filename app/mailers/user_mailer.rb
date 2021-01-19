5839698a146dd89c9128a957cfa234903b76d988
Diego Fernández <difernandez@uc.cl>
2021-01-19 12:16:06 -0300

    fix(user-mailer): change sender to env var

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
   TEMPLATE_ID = ENV.fetch('TEMPLATE_ID')
   SEND_EMAIL = ENV.fetch('SEND_EMAIL')
+  SENDER = ENV.fetch('SENDER')
 
   def send_dynamic_email
-    set_sender('fake_email@bla.com')
+    set_sender(SENDER)
     set_template_id(TEMPLATE_ID)
     dynamic_template_data(key1: 'asd changed')
     mail(to: SEND_EMAIL)
